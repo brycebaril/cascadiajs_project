f686c550383dd19c6ccd31421dc82fdb7e97284e
Bryce Baril <bryce@ravenwall.com>
2014-07-30 08:41:25 -0700

    add html bootstrap, initial dashboard concept

--- client.js
+++ client.js
@@ -1,5 +1,51 @@
-// load flot
-// load isotope
-// set css
-
 // instntiate graphs w/ different query functions
+
+
+
+var terminus = require("terminus")
+
+var Graph = terminus.ctor({objectMode: true}, function (record, encoding, callback) {
+  // draw record
+  callback()
+})
+
+var d1 = [];
+for (var i = 0; i < 14; i += 0.5) {
+  d1.push([i, Math.sin(i)]);
+}
+
+var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+// A null signifies separate line segments
+
+var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+
+// function insertGraph(id) {
+//   var newgraph = document.createElement("div")
+//   newgraph.id = id
+//   newgraph.className = "graph"
+//   $(".container").append(newgraph)
+//   $.plot("#" + id, [d1, d2, d3])
+// }
+//
+// insertGraph("abc")
+// insertGraph("def")
+// insertGraph("qyx")
+// insertGraph("lll")
+//
+//
+// $(".container").isotope({
+//   itemSelector: ".graph",
+// })
+
+$.plot("#r1c1", [d1])
+$.plot("#r1c2", [d1])
+$.plot("#r1c3", [d1])
+
+$.plot("#r2c1", [d1])
+$.plot("#r2c2", [d1])
+$.plot("#r2c3", [d1])
+
+$.plot("#r3c1", [d1])
+$.plot("#r3c2", [d1])
+$.plot("#r3c3", [d1])
--- server.js
+++ server.js
@@ -5,3 +5,13 @@
 // relay data when received from client
 
 // allow range query from client
+"use strict";
+
+var http = require("http")
+var ecstatic = require("ecstatic")
+
+http.createServer(
+  ecstatic({ root: __dirname + "/site" })
+).listen(8080)
+
+console.log("Listening on :8080")
