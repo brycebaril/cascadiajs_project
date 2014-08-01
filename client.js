e139975a95ffbc7d970cdabb3af6c6157576c749
Bryce Baril <bryce@ravenwall.com>
2014-08-01 10:06:50 -0700

    wire client to shoe stream

--- client.js
+++ client.js
@@ -1,12 +1,61 @@
-var TsDB = require("timestreamdb")
+var timestream = require("timestream")
+var through2 = require("through2")
 var level = require("level-js")
 var terminus = require("terminus")
 var multibuffer = require("multibuffer")
+var xtend = require("xtend")
 
 var orig = level("foo", {valueEncoding: "json"})
 var db = TsDB(orig)
 
 var shoe = require("shoe")
+
+var DEFAULT_OPTS = {
+  xaxis: {
+    mode: "time",
+  }
+}
+
+var upstream = through2.obj()
+var tstream = timestream(upstream)
+
+tstream.keep(["photo"])
+  .pipe(graphStream("#r1c1", xtend(DEFAULT_OPTS, {})))
+
+// TODO scale?
+tstream.keep(["accel.x", "accel.y", "accel.z"])
+  .pipe(graphStream("#r1c2", xtend(DEFAULT_OPTS, {})))
+
+tstream.keep(["yellow", "green", "red"])
+  .pipe(graphStream("#r1c3", xtend(DEFAULT_OPTS, {})))
+
+tstream.keep(["photo"])
+  .sma(10)
+  .pipe(graphStream("#r2c1", xtend(DEFAULT_OPTS, {})))
+
+tstream.keep(["accel.x", "accel.y", "accel.z"])
+  .sma(10)
+  .pipe(graphStream("#r2c2", xtend(DEFAULT_OPTS, {})))
+
+tstream.keep(["yellow", "green", "red"])
+  .sum(1000)
+  .pipe(graphStream("#r2c3", xtend(DEFAULT_OPTS, {})))
+
+tstream.keep(["photo"])
+  .dt()
+  .dt()
+  .pipe(graphStream("#r3c1", xtend(DEFAULT_OPTS, {})))
+
+tstream.keep(["accel.x", "accel.y", "accel.z"])
+  .dt()
+  .dt()
+  .pipe(graphStream("#r3c2", xtend(DEFAULT_OPTS, {})))
+
+tstream.keep(["yellow", "green", "red"])
+  .dt()
+  .dt()
+  .pipe(graphStream("#r3c3", xtend(DEFAULT_OPTS, {})))
+
 
 // timestreamdb broken in the browser? just pretend for now?
 var upstream = shoe("/replicate")
@@ -14,51 +63,19 @@ upstream.pipe(terminus.tail(function (mb) {
   var parts = multibuffer.unpack(mb)
   var version = parseInt(parts[0].toString())
   var record = JSON.parse(parts[1].toString())
-  console.log(record)
-  db.put("cjs", record, {version: version}, function (err) {
-    if (err) {
-      console.log(err)
-    }
-  })
+  upstream.write(record)
 }))
 
-var d1 = [];
-for (var i = 0; i < 14; i += 0.5) {
-  d1.push([i, Math.sin(i)]);
+function graphStream(id, options) {
+  var records = []
+  var plot = function () {
+    if (records.length) {
+      $.plot(id, records, options)
+    }
+  }
+  var graphStream = terminus.tail({objectMode: true}, function (record) {
+    records.push([record._t, record])
+    plot()
+  })
+  return graphStream
 }
-
-var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-
-// A null signifies separate line segments
-
-var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-
-// function insertGraph(id) {
-//   var newgraph = document.createElement("div")
-//   newgraph.id = id
-//   newgraph.className = "graph"
-//   $(".container").append(newgraph)
-//   $.plot("#" + id, [d1, d2, d3])
-// }
-//
-// insertGraph("abc")
-// insertGraph("def")
-// insertGraph("qyx")
-// insertGraph("lll")
-//
-//
-// $(".container").isotope({
-//   itemSelector: ".graph",
-// })
-
-$.plot("#r1c1", [d1])
-$.plot("#r1c2", [d1])
-$.plot("#r1c3", [d1])
-
-$.plot("#r2c1", [d1])
-$.plot("#r2c2", [d1])
-$.plot("#r2c3", [d1])
-
-$.plot("#r3c1", [d1])
-$.plot("#r3c2", [d1])
-$.plot("#r3c3", [d1])
